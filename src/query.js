9b8f79bf3f75f60f7a979cbe47fde1ce04a97482
Gediminas Backevicius <gediminas.backevicius@gmail.com>
2016-05-17 16:25:14 +0300

    Quering API has been extended

--- src/query.js
+++ src/query.js
@@ -6,18 +6,36 @@ export default class Query extends QueryBase {
     super(options);
   }
 
-  // TODO: include option to sort
-
   fields(value) {
     return this.setParameter('fields', value);
+  }
+
+  sortBy(value) {
+    // TODO: not implemented
+    return this;
+  }
+
+  sortByDescending(value) {
+    // TODO: not implemented
+    return this;
+  }
+
+  thenBy(value) {
+    // TODO: not implemented
+    return this;
+  }
+
+  thenByDescending(value) {
+    // TODO: not implemented
+    return this;
   }
 
   skip(value) {
     return this.setParameter('skip', value);
   }
 
-  take(value) {
-    return this.setParameter('take', value);
+  limit(value) {
+    return this.setParameter('limit', value);
   }
 
 }
